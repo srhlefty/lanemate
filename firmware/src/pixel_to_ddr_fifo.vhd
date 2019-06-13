6d88385687404093e59f1f1babfb49d74bb992f8
Steven <shunt@akelainc.com>
2019-06-13 11:05:03 -0700

    Now this module is *just* the fifos

--- firmware/src/pixel_to_ddr_fifo.vhd
+++ firmware/src/pixel_to_ddr_fifo.vhd
@@ -74,32 +74,33 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity pixel_to_ddr_fifo is
     Port ( 
-		PCLK : in  STD_LOGIC;                           -- pixel clock
-		PDATA : in  STD_LOGIC_VECTOR (23 downto 0);     -- pixel data
-		P8BIT : in std_logic;                           -- if high, only the lower 8 bits are active (SD 4:2:2)
-		PPUSH : in  STD_LOGIC;                          -- DE
-		PFRAME_ADDR_W : in std_logic_vector(23 downto 0); -- DDR write pointer
-		PFRAME_ADDR_R : in std_logic_vector(23 downto 0); -- DDR read pointer
-		PNEW_FRAME : in std_logic;                      -- pulse to indicate start of frame
-		PRESET_FIFO : in STD_LOGIC;                     -- clear the data and address FIFOs
+		PCLK : in  STD_LOGIC;
+		MCLK : in  STD_LOGIC;
 		
-		-- data-to-write fifo
-		MCLK : in  STD_LOGIC;                             -- memory clock
-		MPOP_W : in  STD_LOGIC;                           -- fifo control
-		MDATA_W : out  STD_LOGIC_VECTOR (255 downto 0);   -- half-burst data (4 high speed clocks worth of data)
-		MADDR_W : out std_logic_vector(23 downto 0);      -- ddr address, high 24 bits
-		MDVALID_W : out  STD_LOGIC;                       -- data valid
+		PDE : in std_logic;
+		PPUSHED : in std_logic;
+		
+		-- write-transaction fifo, input side
+		PADDR_W : in std_logic_vector(23 downto 0);
+		PDATA_W : in std_logic_vector(255 downto 0);
+		PPUSH_W : in std_logic;
+		-- write-transaction fifo, output side
+		MPOP_W : in std_logic;
+		MADDR_W : out std_logic_vector(23 downto 0);    -- ddr address, high 24 bits
+		MDATA_W : out std_logic_vector(255 downto 0);   -- half-burst data (4 high speed clocks worth of data)
+		MDVALID_W : out std_logic;
 
-		-- data-to-read fifo
-		MPOP_R : in  STD_LOGIC;                           -- fifo control
-		MADDR_R : out std_logic_vector(23 downto 0);      -- ddr address, high 24 bits
-		MDVALID_R : out  STD_LOGIC;                       -- data valid
+		-- read-transaction fifo, input side
+		PADDR_R : in std_logic_vector(23 downto 0);
+		PPUSH_R : in std_logic;
+		-- read-transaction fifo, output side
+		MPOP_R : in std_logic;
+		MADDR_R : out std_logic_vector(23 downto 0);    -- ddr address, high 24 bits
+		MDVALID_R : out std_logic;
 
-		-- common interface
-		MLIMIT : in STD_LOGIC_VECTOR (7 downto 0);      -- minimum number of fifo elements for MREADY = 1
+		-- mcb signals
 		MAVAIL : out std_logic_vector(8 downto 0);
-		MFLUSH : out std_logic;
-		MREADY : out  STD_LOGIC
+		MFLUSH : out std_logic
 	);
 end pixel_to_ddr_fifo;
 
@@ -160,189 +161,15 @@ architecture Behavioral of pixel_to_ddr_fifo is
 	  );
 	END COMPONENT;
 	
-	component gearbox8to24 is
-	Port ( 
-		PCLK : in  STD_LOGIC;
-		CE : in  STD_LOGIC;
-		DIN : in  STD_LOGIC_VECTOR (23 downto 0);
-		DE : in  STD_LOGIC;
-		DOUT : out  STD_LOGIC_VECTOR (23 downto 0);
-		DEOUT : out  STD_LOGIC
-	);
-	end component;
-	
-
 	constant ram_addr_width : natural := 9;
 	constant ram_data_width_w : natural := 256 + 24; -- 256 for data, 24 for address
 	constant ram_data_width_r : natural := 24; -- just address
 	
-	signal gearbox_out_w : std_logic_vector(ram_data_width_w-1 downto 0) := (others => '0');
-	signal gearbox_out_r : std_logic_vector(ram_data_width_r-1 downto 0) := (others => '0');
-	
-	signal fifo_push : std_logic := '0';
-	signal fifo_used : std_logic_vector(ram_addr_width-1 downto 0);
-
-
-	signal pdata2 : std_logic_vector(23 downto 0);
-	signal ppush2 : std_logic;
-
-	-- this goes high when DE is low and we've just pushed
-	-- the last elements into the output fifo
 	signal flush_remainder : std_logic := '0';
-	signal flush_d : std_logic := '0';
 
-	signal mready_i : std_logic := '0';
 begin
 
-	Inst_gearbox8to24: gearbox8to24 PORT MAP(
-		PCLK => PCLK,
-		CE => P8BIT,
-		DIN => PDATA,
-		DE => PPUSH,
-		DOUT => pdata2,
-		DEOUT => ppush2
-	);
-
-
-	gearbox : block is
-		type shift_t is array(integer range <>) of std_logic_vector(23 downto 0);
-		signal shifter : shift_t(0 to 31) := (others => (others => '0'));
-		signal word1 : std_logic_vector(255 downto 0) := (others => '0');
-		signal word2 : std_logic_vector(255 downto 0) := (others => '0');
-		signal word3 : std_logic_vector(255 downto 0) := (others => '0');
-		signal count : natural range 0 to 32 := 0;
-		type pusher_state_t is (IDLE, P1, P2, P3);
-		signal pusher_state : pusher_state_t := IDLE;
-		
-		signal base_addr_w : std_logic_vector(23 downto 0) := (others => '0');
-		signal base_addr_r : std_logic_vector(23 downto 0) := (others => '0');
-		-- 1080p has 1080 lines, 180 elements per line, so max address offset is 1080*180/2 = 97,200
-		-- with the /2 because it takes 2 elements to do one burst at the same address. Thus a 17-bit number.
-		signal addr_offset_w : std_logic_vector(16 downto 0) := (others => '0');
-		signal addr_offset_r : std_logic_vector(16 downto 0) := (others => '0');
-		
-		type offset_mode_t is (EVEN, ODD);
-		signal offset_mode : offset_mode_t := EVEN;
-		
-		signal waddr_plus_0 : std_logic_vector(23 downto 0) := (others => '0');
-		signal raddr_plus_0 : std_logic_vector(23 downto 0) := (others => '0');
-		signal waddr_plus_1 : std_logic_vector(23 downto 0) := (others => '0');
-		signal raddr_plus_1 : std_logic_vector(23 downto 0) := (others => '0');
-		signal woffset_plus_1 : std_logic_vector(16 downto 0) := (others => '0');
-		signal roffset_plus_1 : std_logic_vector(16 downto 0) := (others => '0');
-		signal woffset_plus_2 : std_logic_vector(16 downto 0) := (others => '0');
-		signal roffset_plus_2 : std_logic_vector(16 downto 0) := (others => '0');
-		
-		-- I push 3 elements into the fifo at a time, but the address only increments
-		-- every other. So the mechanism for computing the address is slightly nontrivial.
-		-- If the starting address is 'A', then the actual addresses pushed should look
-		-- like this: A,A,A+1,  A+1,A+2,A+2,  A+3,A+3,A+4, etc.
-		-- The pattern repeats after every other group of 3 writes. The offset_mode signal
-		-- keeps track of whether we're going to store using the first pattern (+0,+0,+1)
-		-- or the second pattern (+0,+1,+1). After the 3rd push, I save the new address offset
-		-- and switch the pattern.
-	begin
-		process(PCLK) is
-		begin
-		if(rising_edge(PCLK)) then
-		
-			if(PNEW_FRAME = '1') then
-				base_addr_w <= PFRAME_ADDR_W;
-				base_addr_r <= PFRAME_ADDR_R;
-				addr_offset_w <= (others => '0');
-				addr_offset_r <= (others => '0');
-				offset_mode <= EVEN;
-			else
-			
-		
-				if(ppush2 = '1') then
-					shifter(31) <= pdata2;
-					for i in 0 to 30 loop
-						shifter(i) <= shifter(i+1);
-					end loop;
-					
-					if(count = 32) then
-						-- we've just done a shift so there's already 1
-						count <= 1;
-					else
-						count <= count + 1;
-					end if;
-				else
-					if(count = 32) then
-						count <= 0;
-					end if;
-				end if;
-				
-				-- I need to add the base address to the offset as well as get that value plus 1.
-				-- That's a significant amount of propagation delay, so here I spread out those
-				-- two add operations over 2 clocks.
-				if(count = 31) then
-					waddr_plus_0 <= std_logic_vector(to_unsigned( to_integer(unsigned(base_addr_w)) + to_integer(unsigned(addr_offset_w)) , waddr_plus_0'length));
-					raddr_plus_0 <= std_logic_vector(to_unsigned( to_integer(unsigned(base_addr_r)) + to_integer(unsigned(addr_offset_r)) , raddr_plus_0'length));
-					woffset_plus_1 <= std_logic_vector(to_unsigned( to_integer(unsigned(addr_offset_w)) + 1 , woffset_plus_1'length));
-					roffset_plus_1 <= std_logic_vector(to_unsigned( to_integer(unsigned(addr_offset_r)) + 1 , roffset_plus_1'length));
-					woffset_plus_2 <= std_logic_vector(to_unsigned( to_integer(unsigned(addr_offset_w)) + 2 , woffset_plus_2'length));
-					roffset_plus_2 <= std_logic_vector(to_unsigned( to_integer(unsigned(addr_offset_r)) + 2 , roffset_plus_2'length));
-				end if;
-				
-				if(count = 32) then
-					-- this should happen once by design
-					word1 <= shifter(10)(15 downto 0) & shifter(9) & shifter(8) & shifter(7) & shifter(6) & shifter(5) & shifter(4) & shifter(3) & shifter(2) & shifter(1) & shifter(0);
-					word2 <= shifter(21)(7 downto 0) & shifter(20) & shifter(19) & shifter(18) & shifter(17) & shifter(16) & shifter(15) & shifter(14) & shifter(13) & shifter(12) & shifter(11) & shifter(10)(23 downto 16);
-					word3 <= shifter(31) & shifter(30) & shifter(29) & shifter(28) & shifter(27) & shifter(26) & shifter(25) & shifter(24) & shifter(23) & shifter(22) & shifter(21)(23 downto 8);
-					waddr_plus_1 <= std_logic_vector(to_unsigned( to_integer(unsigned(base_addr_w)) + to_integer(unsigned(woffset_plus_1)) , waddr_plus_1'length));
-					raddr_plus_1 <= std_logic_vector(to_unsigned( to_integer(unsigned(base_addr_r)) + to_integer(unsigned(roffset_plus_1)) , raddr_plus_1'length));
-					pusher_state <= P1;
-				else
-					case pusher_state is
-						when IDLE =>
-							fifo_push <= '0';
-							pusher_state <= IDLE;
-						when P1 =>
-							fifo_push <= '1';
-							gearbox_out_w <= waddr_plus_0 & word1;
-							gearbox_out_r <= raddr_plus_0;
-							pusher_state <= P2;
-						when P2 =>
-							fifo_push <= '1';
-							if(offset_mode = EVEN) then
-								gearbox_out_w <= waddr_plus_0 & word2;
-								gearbox_out_r <= raddr_plus_0;
-							else
-								gearbox_out_w <= waddr_plus_1 & word2;
-								gearbox_out_r <= raddr_plus_1;
-							end if;
-							pusher_state <= P3;
-						when P3 =>
-							fifo_push <= '1';
-							gearbox_out_w <= waddr_plus_1 & word3;
-							gearbox_out_r <= raddr_plus_1;
-							pusher_state <= IDLE;
-							
-							if(offset_mode = EVEN) then
-								addr_offset_w <= woffset_plus_1;
-								addr_offset_r <= roffset_plus_1;
-								offset_mode <= ODD;
-							else
-								addr_offset_w <= woffset_plus_2;
-								addr_offset_r <= roffset_plus_2;
-								offset_mode <= EVEN;
-							end if;
-					end case;
-				end if;
-			
-				if(PPUSH = '0' and pusher_state = P3) then
-					flush_d <= '1';
-				else
-					flush_d <= '0';
-				end if;
-				flush_remainder <= flush_d;
-			
-			end if;
-		end if;
-		end process;
-	
-	end block;
+	flush_remainder <= (not PDE) and PPUSHED;
 
 	flush_cross : pulse_cross_fast2slow PORT MAP(
 		CLKFAST => PCLK,
@@ -351,19 +178,6 @@ begin
 		TRIGOUT => MFLUSH
 	);
 
-	process(MCLK) is
-	begin
-	if(rising_edge(MCLK)) then
-		if(to_integer(unsigned(fifo_used)) >= to_integer(unsigned(MLIMIT)) and to_integer(unsigned(MLIMIT)) > 0) then
-			mready_i <= '1';
-		else
-			mready_i <= '0';
-		end if;
-	end if;
-	end process;
-	MREADY <= mready_i;
-	MAVAIL <= fifo_used;
-
 	
 	writer_fifo_block : block is
 		signal ram_waddr1 : std_logic_vector(ram_addr_width-1 downto 0);
@@ -371,7 +185,8 @@ begin
 		signal ram_raddr2 : std_logic_vector(ram_addr_width-1 downto 0);
 		signal ram_rdata2 : std_logic_vector(ram_data_width_w-1 downto 0);
 		signal ram_we : std_logic;
-		signal bus_tmp : std_logic_vector(ram_data_width_w-1 downto 0);
+		signal bus_in : std_logic_vector(ram_data_width_w-1 downto 0);
+		signal bus_out : std_logic_vector(ram_data_width_w-1 downto 0);
 	begin
 	
 		write_bram: bram_simple_dual_port 
@@ -388,6 +203,9 @@ begin
 			RADDR2 => ram_raddr2,
 			RDATA2 => ram_rdata2
 		);
+		
+		bus_in(ram_data_width_w-1 downto ram_data_width_w-24) <= PADDR_W;
+		bus_in(ram_data_width_w-24-1 downto 0)                <= PDATA_W;
 	
 		write_fifo: fifo_2clk 
 		generic map(
@@ -396,13 +214,13 @@ begin
 		)
 		PORT MAP(
 			WRITE_CLK => PCLK,
-			RESET => PRESET_FIFO,
+			RESET => '0',
 			FREE => open,
-			DIN => gearbox_out_w,
-			PUSH => fifo_push,
+			DIN => bus_in,
+			PUSH => PPUSH_W,
 			READ_CLK => MCLK,
-			USED => fifo_used,
-			DOUT => bus_tmp,
+			USED => MAVAIL,
+			DOUT => bus_out,
 			DVALID => MDVALID_W,
 			POP => MPOP_W,
 			RAM_WADDR => ram_waddr1,
@@ -413,8 +231,8 @@ begin
 			RAM_RDATA => ram_rdata2
 		);
 		
-		MADDR_W <= bus_tmp(ram_data_width_w-1 downto ram_data_width_w-24); -- 24 bits wide
-		MDATA_W <= bus_tmp(ram_data_width_w-24-1 downto 0);                -- 256 bits wide
+		MADDR_W <= bus_out(ram_data_width_w-1 downto ram_data_width_w-24); -- 24 bits wide
+		MDATA_W <= bus_out(ram_data_width_w-24-1 downto 0);                -- 256 bits wide
 		
 	end block;
 
@@ -424,7 +242,8 @@ begin
 		signal ram_raddr2 : std_logic_vector(ram_addr_width-1 downto 0);
 		signal ram_rdata2 : std_logic_vector(ram_data_width_r-1 downto 0);
 		signal ram_we : std_logic;
-		signal bus_tmp : std_logic_vector(ram_data_width_r-1 downto 0);
+		signal bus_in : std_logic_vector(ram_data_width_r-1 downto 0);
+		signal bus_out : std_logic_vector(ram_data_width_r-1 downto 0);
 	begin
 	
 		read_bram: bram_simple_dual_port 
@@ -441,6 +260,8 @@ begin
 			RADDR2 => ram_raddr2,
 			RDATA2 => ram_rdata2
 		);
+		
+		bus_in <= PADDR_R;
 	
 		read_fifo: fifo_2clk 
 		generic map(
@@ -449,13 +270,13 @@ begin
 		)
 		PORT MAP(
 			WRITE_CLK => PCLK,
-			RESET => PRESET_FIFO,
+			RESET => '0',
 			FREE => open,
-			DIN => gearbox_out_r,
-			PUSH => fifo_push,
+			DIN => bus_in,
+			PUSH => PPUSH_R,
 			READ_CLK => MCLK,
 			USED => open,
-			DOUT => bus_tmp,
+			DOUT => bus_out,
 			DVALID => MDVALID_R,
 			POP => MPOP_R,
 			RAM_WADDR => ram_waddr1,
@@ -466,7 +287,7 @@ begin
 			RAM_RDATA => ram_rdata2
 		);
 		
-		MADDR_R <= bus_tmp;
+		MADDR_R <= bus_out;
 		
 	end block;
 
--- firmware/src/pixel_to_ddr_fifo_tb.vhd
+++ firmware/src/pixel_to_ddr_fifo_tb.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   16:24:21 03/26/2019
+-- Create Date:   10:13:36 06/13/2019
 -- Design Name:   
 -- Module Name:   C:/Users/Steven/Documents/Repositories/lanemate/firmware/src/pixel_to_ddr_fifo_tb.vhd
 -- Project Name:  firmware
@@ -40,207 +40,210 @@ ARCHITECTURE behavior OF pixel_to_ddr_fifo_tb IS
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT pixel_to_ddr_fifo
-    Port ( 
-		PCLK : in  STD_LOGIC;                           -- pixel clock
-		PDATA : in  STD_LOGIC_VECTOR (23 downto 0);     -- pixel data
-		P8BIT : in  STD_LOGIC;                          -- if high, only the lower 8 bits are active (SD 4:2:2)
-		PPUSH : in  STD_LOGIC;                          -- DE
-		PFRAME_ADDR_W : in std_logic_vector(23 downto 0); -- DDR write pointer
-		PFRAME_ADDR_R : in std_logic_vector(23 downto 0); -- DDR read pointer
-		PNEW_FRAME : in std_logic;                      -- pulse to indicate start of frame
-		PRESET_FIFO : in STD_LOGIC;                     -- clear the data and address FIFOs
-		
-		-- data-to-write fifo
-		MCLK : in  STD_LOGIC;                             -- memory clock
-		MPOP_W : in  STD_LOGIC;                           -- fifo control
-		MDATA_W : out  STD_LOGIC_VECTOR (255 downto 0);   -- half-burst data (4 high speed clocks worth of data)
-		MADDR_W : out std_logic_vector(23 downto 0);      -- ddr address, high 24 bits
-		MDVALID_W : out  STD_LOGIC;                       -- data valid
-
-		-- data-to-read fifo
-		MPOP_R : in  STD_LOGIC;                           -- fifo control
-		MADDR_R : out std_logic_vector(23 downto 0);      -- ddr address, high 24 bits
-		MDVALID_R : out  STD_LOGIC;                       -- data valid
-
-		-- common interface
-		MLIMIT : in STD_LOGIC_VECTOR (7 downto 0);      -- minimum number of fifo elements for MREADY = 1
-		MAVAIL : out std_logic_vector(8 downto 0);
-		MFLUSH : out  STD_LOGIC;
-		MREADY : out  STD_LOGIC
-	);
+    PORT(
+         PCLK : IN  std_logic;
+         MCLK : IN  std_logic;
+         PDE : IN  std_logic;
+         PPUSHED : IN  std_logic;
+         PADDR_W : IN  std_logic_vector(23 downto 0);
+         PDATA_W : IN  std_logic_vector(255 downto 0);
+         PPUSH_W : IN  std_logic;
+         MPOP_W : IN  std_logic;
+         MADDR_W : OUT  std_logic_vector(23 downto 0);
+         MDATA_W : OUT  std_logic_vector(255 downto 0);
+         MDVALID_W : OUT  std_logic;
+         PADDR_R : IN  std_logic_vector(23 downto 0);
+         PPUSH_R : IN  std_logic;
+         MPOP_R : IN  std_logic;
+         MADDR_R : OUT  std_logic_vector(23 downto 0);
+         MDVALID_R : OUT  std_logic;
+         MAVAIL : OUT  std_logic_vector(8 downto 0);
+         MFLUSH : OUT  std_logic
+        );
     END COMPONENT;
     
-	component ddr_to_pixel_fifo is
-    Port ( PCLK : in  STD_LOGIC;
-           PDATA : out  STD_LOGIC_VECTOR (23 downto 0);
-			  P8BIT : in std_logic;
-           PPOP : in  STD_LOGIC;
-			  PDVALID : out STD_LOGIC;
-			  PRESET : in STD_LOGIC;
-           MCLK : in  STD_LOGIC;
-			  MRESET : in STD_LOGIC;
-           MPUSH : in  STD_LOGIC;
-           MDATA : in  STD_LOGIC_VECTOR (255 downto 0)
-           );
-	end component;
-	
-	component internal_mcb is
-	Port ( 
-		MCLK : in std_logic;
-		TRANSACTION_SIZE : in std_logic_vector(7 downto 0); -- number of fifo elements to read/write at once
-		
-		-- interface common to both fifos
-		MREADY : in std_logic;
-		MFLUSH : in std_logic;
-		MAVAIL : in std_logic_vector(8 downto 0);
-		
-		-- interface to data-to-write fifo
-		MPOP_W : out std_logic;
-		MDATA_W : in std_logic_vector(255 downto 0);  -- half-burst data (4 high speed clocks worth of data)
-		MADDR_W : in std_logic_vector(23 downto 0);   -- ddr address, high 24 bits
-		MDVALID_W : in std_logic;                      -- data valid
-		
-		-- interface to data-to-read fifo
-		MPOP_R : out std_logic;
-		MADDR_R : in std_logic_vector(23 downto 0);   -- ddr address, high 24 bits
-		MDVALID_R : in std_logic;
-		
-		-- interface to data-just-read fifo
-		MPUSH : out std_logic;
-		MDATA_R : out std_logic_vector(255 downto 0)
-	);
-	end component;
 
    --Inputs
    signal PCLK : std_logic := '0';
-   signal PDATA : std_logic_vector(23 downto 0) := (others => '0');
-	signal P8BIT : std_logic := '1';
-   signal PFRAME_ADDR_W : std_logic_vector(23 downto 0) := std_logic_vector(to_unsigned(0, 24));
-   signal PFRAME_ADDR_R : std_logic_vector(23 downto 0) := std_logic_vector(to_unsigned(0, 24));
-   signal PPUSH : std_logic := '0';
-   signal PNEW_FRAME : std_logic := '0';
-   signal PRESET_FIFO : std_logic := '0';
    signal MCLK : std_logic := '0';
-	-- both HD: 30 elements per transaction, makes 15 ddr bursts
-   signal MLIMIT : std_logic_vector(7 downto 0) := x"2f";
-   signal MAVAIL : std_logic_vector(8 downto 0);
+   signal PDE : std_logic := '0';
+   signal PPUSHED : std_logic := '0';
+   signal PADDR_W : std_logic_vector(23 downto 0) := (others => '0');
+   signal PDATA_W : std_logic_vector(255 downto 0) := (others => '0');
+   signal PPUSH_W : std_logic := '0';
+   signal MPOP_W : std_logic := '0';
+   signal PADDR_R : std_logic_vector(23 downto 0) := (others => '0');
+   signal PPUSH_R : std_logic := '0';
+   signal MPOP_R : std_logic := '0';
 
-
-	signal MPOP_W : std_logic := '0';
-	signal MPOP_R : std_logic := '0';
-	
  	--Outputs
-   signal MDATA_W : std_logic_vector(255 downto 0);
    signal MADDR_W : std_logic_vector(23 downto 0);
-   signal MADDR_R : std_logic_vector(23 downto 0);
+   signal MDATA_W : std_logic_vector(255 downto 0);
    signal MDVALID_W : std_logic;
+   signal MADDR_R : std_logic_vector(23 downto 0);
    signal MDVALID_R : std_logic;
-   signal MREADY : std_logic;
+   signal MAVAIL : std_logic_vector(8 downto 0);
    signal MFLUSH : std_logic;
-	
-	signal pdata_out : std_logic_vector(23 downto 0);
-	signal ppop : std_logic := '0';
-	signal pdatavalid : std_logic;
 
 	signal count : natural := 0;
-
-
-	signal mcb_push_out : std_logic;
-	signal mcb_push_data : std_logic_vector(255 downto 0);
-	
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: pixel_to_ddr_fifo PORT MAP (
           PCLK => PCLK,
-          PDATA => PDATA,
-          P8BIT => P8BIT,
-          PPUSH => PPUSH,
-          PFRAME_ADDR_W => PFRAME_ADDR_W,
-          PFRAME_ADDR_R => PFRAME_ADDR_R,
-          PNEW_FRAME => PNEW_FRAME,
-          PRESET_FIFO => PRESET_FIFO,
           MCLK => MCLK,
+          PDE => PDE,
+          PPUSHED => PPUSHED,
+          PADDR_W => PADDR_W,
+          PDATA_W => PDATA_W,
+          PPUSH_W => PPUSH_W,
           MPOP_W => MPOP_W,
-          MDATA_W => MDATA_W,
           MADDR_W => MADDR_W,
+          MDATA_W => MDATA_W,
           MDVALID_W => MDVALID_W,
-			 MPOP_R => MPOP_R,
-			 MADDR_R => MADDR_R,
-			 MDVALID_R => MDVALID_R,
-          MLIMIT => MLIMIT,
-			 MAVAIL => MAVAIL,
-			 MFLUSH => MFLUSH,
-          MREADY => MREADY
+          PADDR_R => PADDR_R,
+          PPUSH_R => PPUSH_R,
+          MPOP_R => MPOP_R,
+          MADDR_R => MADDR_R,
+          MDVALID_R => MDVALID_R,
+          MAVAIL => MAVAIL,
+          MFLUSH => MFLUSH
         );
 
---	Inst_internal_mcb: internal_mcb PORT MAP(
---		MCLK => MCLK,
---		TRANSACTION_SIZE => MLIMIT,
---		MREADY => MREADY,
---		MFLUSH => MFLUSH,
---		MAVAIL => MAVAIL,
---		MPOP_W => MPOP_W,
---		MDATA_W => MDATA_W,
---		MADDR_W => MADDR_W,
---		MDVALID_W => MDVALID_W,
---		MPOP_R => MPOP_R,
---		MADDR_R => MADDR_R,
---		MDVALID_R => MDVALID_R,
---		MPUSH => mcb_push_out,
---		MDATA_R => mcb_push_data
---	);
-
---	Inst_ddr_to_pixel_fifo: ddr_to_pixel_fifo PORT MAP(
---		PCLK => PCLK,
---		PDATA => pdata_out,
---		P8BIT => P8BIT,
---		PPOP => ppop,
---		PDVALID => pdatavalid,
---		PRESET => '0',
---		MCLK => MCLK,
---		MRESET => '0',
---		MPUSH => mcb_push_out,
---		MDATA => mcb_push_data
---	);
-
-	--PCLK <= not PCLK after 3.367 ns; -- 1080p
 	PCLK <= not PCLK after 6.73 ns; -- 720p
-	--PCLK <= not PCLK after 18.519 ns; -- 480i
-	MCLK <= not MCLK after 5 ns; -- 100MHz
-
+	MCLK <= not MCLK after 5 ns; -- 100 MHz
+	
+	
 	process(PCLK) is
-		variable n : std_logic_vector(7 downto 0);
-		--constant line_length : natural := 1920;
-		--constant line_length : natural := 1280;
-		constant line_length : natural := 1440;
-		constant readout_delay : natural := line_length/2;
 	begin
 	if(rising_edge(PCLK)) then
 		count <= count + 1;
-		if(count = 2) then
-			PNEW_FRAME <= '1';
+		if(count >= 10 and count < 50) then
+			PDE <= '1';
 		else
-			PNEW_FRAME <= '0';
-		end if;
-
-		if((count >= 10 and count < 10+line_length)) then
-			n := std_logic_vector(to_unsigned(count-10, 8));
-			PDATA <= n & n & n;
-			PPUSH <= '1';
-		else
-			PDATA <= (others => '0');
-			PPUSH <= '0';
+			PDE <= '0';
 		end if;
 		
---		if(count >= 10+readout_delay and count < 10+readout_delay+line_length) then
---			ppop <= '1';
---		else
---			ppop <= '0';
---		end if;
+		if(count = 12) then
+			PADDR_W <= x"000000";
+			PDATA_W <= x"1F1E1D1C1B1A191817161514131211100F0E0D0C0B0A09080706050403020100";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 13) then
+			PADDR_W <= x"000000";
+			PDATA_W <= x"3F3E3D3C3B3A393837363534333231302F2E2D2C2B2A29282726252423222120";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 14) then
+			PADDR_W <= x"000001";
+			PDATA_W <= x"5F5E5D5C5B5A595857565554535251504F4E4D4C4B4A49484746454443424140";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 15) then
+			PPUSH_W <= '0';
+			PPUSH_R <= '0';
+			PPUSHED <= '1';
+			
+		elsif(count = 30) then
+			PADDR_W <= x"000001";
+			PDATA_W <= x"1F1E1D1C1B1A191817161514131211100F0E0D0C0B0A09080706050403020100";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 31) then
+			PADDR_W <= x"000002";
+			PDATA_W <= x"3F3E3D3C3B3A393837363534333231302F2E2D2C2B2A29282726252423222120";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 32) then
+			PADDR_W <= x"000002";
+			PDATA_W <= x"5F5E5D5C5B5A595857565554535251504F4E4D4C4B4A49484746454443424140";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 33) then
+			PPUSH_W <= '0';
+			PPUSH_R <= '0';
+			PPUSHED <= '1';
+			
+		elsif(count = 50) then
+			PADDR_W <= x"000001";
+			PDATA_W <= x"1F1E1D1C1B1A191817161514131211100F0E0D0C0B0A09080706050403020100";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 51) then
+			PADDR_W <= x"000002";
+			PDATA_W <= x"3F3E3D3C3B3A393837363534333231302F2E2D2C2B2A29282726252423222120";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 52) then
+			PADDR_W <= x"000002";
+			PDATA_W <= x"5F5E5D5C5B5A595857565554535251504F4E4D4C4B4A49484746454443424140";
+			PPUSH_W <= '1';
+			PADDR_R <= x"000000";
+			PPUSH_R <= '1';
+			PPUSHED <= '0';
+		elsif(count = 53) then
+			PPUSH_W <= '0';
+			PPUSH_R <= '0';
+			PPUSHED <= '1';
+			
+		else
+			PPUSH_W <= '0';
+			PPUSH_R <= '0';
+			PPUSHED <= '0';
+		end if;
 		
 	end if;
 	end process;
 	
+	mcb_sim : block is
+		type state_t is (WAITING, POPPING, REST);
+		signal state : state_t := WAITING;
+		constant limit : natural := 2;
+		signal popcount : natural := 0;
+	begin
 	
+		process(MCLK) is
+		begin
+		if(rising_edge(MCLK)) then
+		case state is
+		when WAITING =>
+			MPOP_W <= '0';
+			if(to_integer(unsigned(MAVAIL)) >= limit) then
+				popcount <= 0;
+				state <= POPPING;
+			end if;
+		
+		when POPPING =>
+			popcount <= popcount + 1;
+			if(popcount = limit) then
+				MPOP_W <= '0';
+				state <= REST;
+			else
+				MPOP_W <= '1';
+			end if;
+			
+		when REST =>
+			state <= WAITING;
+		end case;
+		end if;
+		end process;
+	
+	end block;
+
 END;
